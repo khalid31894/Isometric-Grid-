0d64d436b6e3a67b43796e9da680d77078e6573d
Khalid Ahmad <khalid.ahmad@hazelmobile.co>
2024-05-01 16:51:38 +0500

    Import Unity + Assignment 2 Assets

---
+++ Assets/IsometricGrid/Scripts/Test.cs
@@ -0,0 +1,24 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    [Button]
+    private void TestFunc()
+    {
+        Debug.Log("Hi");    
+    }
+}
