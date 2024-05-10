ae03329c83e5525831f5c89d819f2a7350f67514
Khalid Ahmad <khalid.ahmad@hazelmobile.co>
2024-05-10 23:48:37 +0500

    Revert "Revert "Load from Json""
    This reverts commit 32b23fc4f016eb74937617af73d2ff481e6b64ab.

--- Assets/IsometricGrid/Scripts/TerrainLoader.cs
+++ Assets/IsometricGrid/Scripts/TerrainLoader.cs
@@ -1,8 +1,34 @@
+using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TerrainLoader : MonoBehaviour
 {
+    public string jsonFlePath = "data";
+    public Terrain LoadTerrain()
+    {
+        Terrain _terrain = new Terrain();
 
+        TextAsset jsonTextAsset = Resources.Load<TextAsset>(jsonFlePath);
+        if (jsonTextAsset == null)
+        {
+            Debug.LogError("Loading Failed ");
+            return null;
+        }
+
+        string jsonContent = jsonTextAsset.text;
+        if (string.IsNullOrEmpty(jsonContent))
+        {
+            Debug.LogError("File is IsNullOrEmpty");
+            return null;
+        }
+
+        _terrain = JsonConvert.DeserializeObject<Terrain>(jsonContent);
+
+        
+        Debug.Log("LoadJSON: " + _terrain);
+
+        return _terrain;
+    }
 }
