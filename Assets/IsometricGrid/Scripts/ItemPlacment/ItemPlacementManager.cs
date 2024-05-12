e55e14157b694f74e72da5bc0d7a143567dcbfc9
Khalid Ahmad <khalid.ahmad@hazelmobile.co>
2024-05-12 23:02:58 +0500

    Mouse Input actions defined

--- Assets/IsometricGrid/Scripts/ItemPlacment/ItemPlacementManager.cs
+++ Assets/IsometricGrid/Scripts/ItemPlacment/ItemPlacementManager.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class ItemPlacementManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private bool IsPlaceable(Tile tile, Item item)
     {
-        
+        return false;
     }
 }
