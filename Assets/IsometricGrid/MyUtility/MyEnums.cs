9619b9de80a11ef84ad01a8ce2812d8cc3cf8c51
Khalid Ahmad <khalid.ahmad@hazelmobile.co>
2024-05-12 22:06:01 +0500

    Item and Tile prefabs

---
+++ Assets/IsometricGrid/MyUtility/MyEnums.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyEnums 
+{
+    public enum TileType
+    {
+        Dirt,
+        Grass,
+        Stone,
+        Wood,
+    }
+}
--- Assets/IsometricGrid/Scripts/Tile.cs
+++ Assets/IsometricGrid/Scripts/Tile.cs
@@ -1,9 +1,26 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-  
+    [SerializeField] private MyEnums.TileType tileType;
+    [SerializeField] private SpriteRenderer tileSprite;
+    [SerializeField] private bool isOccupied = false;
+    public void SetOccupied(bool occupied)
+    {
+        isOccupied = occupied;
+    }
+
+    public bool IsOccupied()
+    {
+        return isOccupied;
+    }
+
+    public void HighlightTile()
+    {
+        
+    }
 }
