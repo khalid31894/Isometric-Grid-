c75730be26b3676b0a1b573d0168511b00e3143b
Khalid Ahmad <khalid.ahmad@hazelmobile.co>
2024-05-12 22:26:41 +0500

    Item Placement Manager

--- Assets/IsometricGrid/Scripts/Item.cs
+++ Assets/IsometricGrid/Scripts/Item.cs
@@ -5,4 +5,11 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     [SerializeField] private int area=0;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private MyEnums.TileType placableOnTile;
+    private void OnEnable()
+    {
+        spriteRenderer.sortingOrder = Mathf.RoundToInt(-transform.position.y * 100);
+
+    }
 }
