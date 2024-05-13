4dd8f88f5e25a71149b58d5a34baa329a6b68ad4
Khalid Ahmad <khalid.ahmad@hazelmobile.co>
2024-05-13 09:29:28 +0500

    item layering

--- Assets/IsometricGrid/Scripts/Item.cs
+++ Assets/IsometricGrid/Scripts/Item.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    [SerializeField] private int area=0;
     [SerializeField] private SpriteRenderer spriteRenderer;
-    [SerializeField] private MyEnums.TileType placableOnTile;
+    
+    public int area=0;
+    public MyEnums.TileType placableOnTile;
     private void OnEnable()
+    {
+        MangeItemLayering();
+    }
+
+    private void MangeItemLayering()
     {
         spriteRenderer.sortingOrder = Mathf.RoundToInt(-transform.position.y * 100);
 
